c5f8ad6e13bf38b472f14cc0607bffc3ae22c349
Vladimir Torgashov <tvn_job@rambler.ru>
2015-07-07 12:25:17 +0300

    comment3

--- TestCalc.java
+++ TestCalc.java
@@ -12,7 +12,7 @@ import org.junit.runners.Parameterized;
 import static org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-//add comment
+//add comment3
 public class TestCalc {
     // Обратите внимание на данные в скобках - первый два - числа, которые складываем/вычитаем
     // Вторые - это их сумма и разность
